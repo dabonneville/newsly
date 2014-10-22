015b7bd1536f2077ddf559984fa2cdbc4289d1e4
zahra <zahratee@gmail.com>
2014-10-22 03:48:40 +0100

    get all and keywords working now

--- backend/server.js
+++ backend/server.js
@@ -25,15 +25,21 @@ db.once('open', function() {
 	Preference = mongoose.model('Preference', PreferenceSchema);
 
 
-
 });
 
 mongoose.connect('mongodb://localhost/test');
 
 
 function saveNewsToDb(obj){
-	$ = cheerio.load(obj.fields.trailText);
-	var trail = $(obj.fields.trailText).text();
+	console.log(obj.fields.trailText);
+	var $, trail;
+	try {
+		$ = cheerio.load(obj.fields.trailText);
+		trail = $.text();
+	}
+	catch(err){
+		console.log(err);
+	}
 	var article = new Article({
 		id: obj.id,
 		picurl: extractImageURI(obj.fields.main),
@@ -44,47 +50,90 @@ function saveNewsToDb(obj){
 
 	article.save(function(err, article) {
 		if (err) return console.error(err);
-		console.dir(article);
 	});
 }
 
-function savePrefNewsToDb(obj){
-	$ = cheerio.load(obj.fields.trailText);
-	var trail = $(obj.fields.trailText).text();
-	var article = new Article({
-		id: obj.id,
-		picurl: extractImageURI(obj.fields.main),
-		headline: obj.fields.headline,
-		trailtext: trail,
-		url: obj.webUrl
-	});
+/*function savePrefNewsToDb(obj){
+
+	var $;
+
+	try{
+		console.log(obj.fields.trailText);
+		$ = cheerio.load(obj.fields.trailText);
+	}
+	catch(err){
+		console.log("cheerio problem 1 " + err);
+	}
+	
+
+	try{
+		trail = $(obj.fields.trailText).text();
+	}
+	catch(err){
+		console.log("cheerio problem " + err);
+	}
+	
+	try {
+		console.log(extractImageURI(obj.fields.main));
+	}
+	catch(err){
+		console.log("1 " + err);
+	}
+	try {
+		console.log(obj.fields.headline);
+	}
+	catch(err){
+		console.log("2 " + err);
+	}
+	try {
+		console.log(extractImageURI(obj.fields.main));
+	}
+	catch(err){
+		console.log("3 " + err);
+	}
+	try {
+		console.log(trail);
+	}
+	catch(err){
+		console.log("4 " + err);
+	}
+	try {
+		console.log(obj.webUrl);
+	}
+	catch(err){
+		console.log("5 " + err);
+	}
+	try{
+		var article = new Article({
+			id: obj.id,
+			picurl: extractImageURI(obj.fields.main),
+			headline: obj.fields.headline,
+			trailtext: trail,
+			url: obj.webUrl
+		});
+	}
+	catch(err){
+		console.log("OH SHIT THERE'S ANOTHER SHITTY ERROR ON MAKING ARTICLE " + err);
+	}
 
 	article.save(function(err, article) {
-		if (err) return console.error(err);
+		if (err) console.error( "OH SHIT THERE IS AN ERROR!!!! " + err);
+
 		//console.dir(article);
 	});
-}
+}*/
 
 function fetchNews(req,res){
 	var data = request('http://content.guardianapis.com/search?api-key=t3myqd7scnfu4t5w8zp7jx4v&show-fields=headline,trailText,main&page=3&page-size=10', function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var jsonData = JSON.parse(body);
-			console.log(jsonData.response.results);
 			for (var i = 0; i < jsonData.response.results.length; i++){
+				console.log(i);
 				saveNewsToDb(jsonData.response.results[i]);
 			}
-			//saveNewsToDb(jsonData.response.results);
-			//res.send(jsonData.response.results);
 		}
 	});
 }
-
-function getAllNews(req,res){
-	Article.find(function(err,data){
-		res.send(data);
-	})
-}
-
 
 function getPrefNews(req,res){
 	var prefs = Preference.find(function(err, preference){
@@ -93,51 +142,54 @@ function getPrefNews(req,res){
 		for (var i = 0; i< preference.length; i++){
 			strarray.push(preference[i].key);
 		}
-		console.log()
-		var data = request('http://content.guardianapis.com/search?api-key=t3myqd7scnfu4t5w8zp7jx4v&show-fields=headline,trailText,main&page-size=100&q='+strarray.toString(), function (error, response, body) {
+		var data = request('http://content.guardianapis.com/search?api-key=t3myqd7scnfu4t5w8zp7jx4v&show-fields=headline,trailText,main&page-size=10&q='+strarray.toString(), function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var prefData = JSON.parse(body);
-				for (var i = 0; i < prefData.response.results.length; i++){
+				//console.log(prefData.response.results);
+				for (var i = 3; i < prefData.response.results.length; i++){
 					savePrefNewsToDb(prefData.response.results[i]);
-					console.log(i + prefData.response.results[i].id);
+					console.log("Done " + i + " " + prefData.response.results[i].id);
 				}				
 			}
 		});
 	});
 }
 
-
 function addPrefs(req, res){
 	var incomingID = req.params.id;
-	Article.findOne( {_id: incomingID}, function(err, data) {
-		if (err) return console.error(err);
-		//res.send(data.content);
-		getKeywords(data.content);
+	Article.findOne( {"_id": incomingID}, function(err, data) {
+		var searchtext = data.headline + " " + data.trailtext;
+			return getKeywords(searchtext);
+
 	});
+
+	res.send(incomingID);
+
 }
 
 
-function getKeywords(content){
+function getKeywords(text){
+	var data;
 	unirest.post("https://joanfihu-article-analysis-v1.p.mashape.com/text")
 	.header("X-Mashape-Key", "2LKLhCuMs2mshs6s3OxvtL2325czp1JNAz8jsnz6QtbmGesuEv")
 	.header("Content-Type", "application/x-www-form-urlencoded")
-	.field("text", content)
-	.field('title', 'hi')
-	.end(function (result) {	
-		//console.log(result.status, result.headers, result.body);
-		//res.send(result);
-		
-
+	.field("text", text)
+	.field("title", " ")
+	.end(function (result) {
+		data = result.body.keywords[0];
 		var preference = new Preference({
-			key:result.body.keywords[0]
+				key:data
 		});
 
 		preference.save(function(err, preference){
 			if (err) console.error(err);
 			console.dir(preference);
+			console.log("pref saved");
 		});
+		return data;
 	});
 }
+
 
 
 function extractImageURI(main){
@@ -148,11 +200,10 @@ function extractImageURI(main){
 }
 
 var server = restify.createServer();
-server.get('/getallnews', getAllNews);
-server.get('/fetchNews', getPrefNews);
+server.get('/getAllNews', fetchNews);
+//server.get('/getPrefNews', getPrefNews);
+//server.get('/fetchNews', fetchNews);
 server.get('/addPrefs/:id', addPrefs)
-//server.get('');
-
 
 
 server.listen(8080, function() {
